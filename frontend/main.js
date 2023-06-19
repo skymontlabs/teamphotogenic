87b383feea2ea32ad5e8cc38a69513c26a55b80a
Stephen Liang <sl@sent.at>
2023-06-18 23:26:28 -0700

    general refresh

--- frontend/main.js
+++ frontend/main.js
@@ -13,7 +13,6 @@ displayNotificationsV.onclick = (event) => {cur.style.display='none';displayNoti
 helpSupportV.onclick = (event)          => {cur.style.display='none';helpSupportPanel.style.display='block';cur=helpSupportPanel}
 dark.onclick = (event) => {
     if (loggedin && event.target == dark) {
-        //alert('del')
         dark.style.display='none'
         indvImg.style.display='none'
     }
@@ -26,31 +25,33 @@ buyCredLi.onclick = (event) => {
 }
 
 signuper.onclick = (event) => {
-    dark.classList.add('overlay')
-    dark.style.display='none';
-    loginer.style.display='none';
-    loggedin=true;
-
-    //scorerater.style.display='block';
-    globl.className='showS'
-    nV.style.display='block';
-
-    //emailSignup.innerText
-
-    //sock.send(seedCode->0 + email.length + email)
-
-
-    // WAIT
-
-
     const seed = new Uint8Array(16); // get this from the backend, async wait from X
+    
+    //progress.classList.add('active')
+        
     argon2.hash({
       pass: passwordSignup.innerText, salt: seed,
-      time: 24, mem: 16384, hashLen: 64,
+      time: 32, mem: 16384, hashLen: 64,
       type: argon2.ArgonType.Argon2d
     })
-    .then(h => {alert(h.hashHex)})
-    .catch(e => console.error('sex'))
+    .then(h => {
+        // login now
+        //sock.send(seedCode->0 + email.length + email)
+
+        dark.classList.add('overlay')
+        dark.style.display='none';
+        loginer.style.display='none';
+        loggedin=true;
+
+
+        //scorerater.style.display='block';
+        globl.className='showS'
+        nV.style.display='block';
+    })
+    .catch(e => {
+        wrongpass.style.display='block'
+        console.error('sex')
+    })
 }
 
 
@@ -183,6 +184,7 @@ function get(url) {
 const links = document.getElementsByTagName('a');
 for(let i = 0; i < links.length; i++) {
   links[i].addEventListener('click', function(event) {
+    console.log('lauren')
     event.preventDefault();
     get(links[i].href);
 
@@ -191,7 +193,6 @@ for(let i = 0; i < links.length; i++) {
     if (prfChx.checked) {
         prfChx.checked=false
     }
-
   }, false);
 }
 
@@ -299,3 +300,382 @@ sortBayes.onclick = (event) => {sortMethod.innerText='Bayesian Approximation';so
 sortQuart.onclick = (event) => {sortMethod.innerText='Lower quartile score';sortCheckbox.checked=false}
 sortDate.onclick = (event) => {sortMethod.innerText='Date added';sortCheckbox.checked=false}
 sortModi.onclick = (event) => {sortMethod.innerText='Date modified';sortCheckbox.checked=false}
+
+
+
+const sck = new WebSocket("wss://api.teamphotogenic.com/ws", "protocolOne");
+sck.binaryType = "arraybuffer";
+//sck.onopen = onopen;
+//sck.onmessage = onmessage;
+sck.onclose = onclose;
+sck.onerror = onerror;
+
+seed = null
+
+onbeforeunload = (event) => {console.log('seggs')};
+
+
+sck.onopen = (event) => {
+}
+
+sck.onmessage = (event) => {
+const f = document.getElementById("chatbox").contentDocument;
+const msg = Buffer(event.data);
+
+switch (msg[0]) {
+  case 0: // get seed
+    seed = ArrayBuffer(msg, 1, 20)
+    break;
+  case 1: // login
+    msg = data
+
+    localStorage.setItem("token", ArrayBuffer(msg, 1));
+    break;
+  case 2: // signup
+    "Username exists"
+
+    "Check code"
+    break;
+  case 3: // submit totp 
+    let password = ArrayBuffer(msg, 1)
+
+    // goes to login
+
+    break;
+  case 4: // init change password
+    // get old seed and new seed
+    let oldSeed = ArrayBuffer(msg, 1)
+    let newSeed = ArrayBuffer(msg, 1)
+
+    changePassword(contact, oldPassword, newPassword, oldSeed, newSeed)
+    break;
+  case 5: // change password
+    "Password changed"
+    break;
+  case 6: // verify acct
+    msg = data
+
+    localStorage.setItem("token", ArrayBuffer(msg, 1));
+    break;
+  case 7: // init totp 
+    "";
+    break;
+  case 8: // add totp
+    "";
+    break;
+  case 9: // remove totp
+    "TOTP Removed"
+    break;
+
+/*
+1. GetTasks for all tasks
+2. GetTaskId for task of certain id with images
+3. GetImageId for just 1 image data
+4. RateIndiv for paired rating. Returns 16 images. Will be 8 different sets.
+5. RatePairs for paired rating. Returns 16 images. Will be 8 different sets.
+6. CreateSequence, creates a new sequence. This also activates a test.
+7. DeleteSequence - deletes a certain sequence
+8. ActivateTest, creates a new test for an already created sequence.
+9. StopTest - stops a certain test
+10. GetUsers -  get data from all users, for admin only
+11. GetUser - get data from a user
+*/
+
+  case 10: // GetTasks
+    // parse all the tasks
+    break;
+  case 11: // GetTaskID
+    // parse all the images
+    break;
+  case 12: // RateIndiv
+    // rate individual image [group of 8], and get 8 back
+
+    break;
+  case 13: // RatePairs
+    // rate individual image [group of 16, or 8 pairs]
+
+    break;
+  case 14: // CreateSequence
+    // Now go to the sequenceID/taskID page
+    break;
+
+  case 15: // DeleteSequence
+    // say it was successful
+    // Go to all tasks
+    break;
+
+  case 16: // Activate task
+    // say it was successful
+    break;
+
+  case 17: // Stop task
+    // say it was successful
+    break;
+  
+  case 18: // Get user
+    // get your own user data, like the extensions bought + the 
+    break;
+}
+};
+
+function convert2int0(Uint8Arr) {
+  var length = Uint8Arr.length;
+
+  let buffer = Buffer.from(Uint8Arr);
+  var result = buffer.readUIntBE(0, length);
+
+  return result;
+}
+
+function convert2int0(buff) {
+  var view = new DataView(buff.buffer, 0);
+  view.getUint32(0, true); // true here represents little-endian
+}
+
+function parseImageset()
+{
+//begins with 1 byte # of imagesets listed
+
+/*const uint8 = new Uint8Array([10, 20, 30, 40, 50]);
+const array1 = uint8.slice(1, 3);
+
+console.log(array1);
+// Expected output: Uint8Array [20, 30]
+*/
+
+for (var i = 0; i < 20; ++i) {
+  /*[0:8] - photoset ID
+  [8:12] - total num images
+  [12:16] - total num votes
+  [12:16] - total num comments
+  [12:16] - score
+
+  [16:24] - imageID 1, HEX 
+  [16:24] - url 1, HEX*/
+}
+
+}
+
+function parseRate()
+{
+for (let i = 0; i < 8; ++i) {/*
+  // url for image
+  [0:8] - imageID
+  [8:24] - url*/
+}
+}
+
+function parsePair()
+{
+for (let i = 0; i < 8; ++i) {
+  // url for image
+  /*
+  [0:8] - imageID 1
+  [8:24] - url 1
+  [24:32] - imageID 2
+  [32:48] - url 2*/
+}
+}
+
+function parse()
+{
+for (let i = 0; i < 8; ++i) {
+  // url for image
+  /*
+  [0:8] - imageID 1
+  [8:24] - url 1
+  [24:32] - imageID 2
+  [32:48] - url 2*/
+}
+}
+
+function getSeed()
+{
+let contact = contactInput.innerText
+let buff = new ArrayBuffer(3 + contact.length);
+
+buff[0] = 1;
+buff[1] = contact.length;
+buff[2] = contact[0] == '+';
+
+for (var i = 0; i < contact.length; ++i)
+  buff[i + 3] = contact[i]
+
+sk.send(buff,{binary:true});
+}
+
+function signup(contact, password, seed)
+{
+// same as login
+let buff = new ArrayBuffer([1 + contact.length + 64]);
+
+argon2.hash({
+  pass: password, salt: seed,
+  time: 32, mem: 32768, hashLen: 64,
+  type: argon2.ArgonType.Argon2d
+})
+.then(h => {alert(h.hashHex)})
+.catch(e => console.error('Signup did not work'))
+
+sk.send(buff,{binary:true});
+}
+
+function login(contact, password, seed)
+{
+let buff = new ArrayBuffer([1 + contact.length + 64]);
+
+argon2.hash({
+  pass: password, salt: seed,
+  time: 32, mem: 32768, hashLen: 64,
+  type: argon2.ArgonType.Argon2d
+})
+.then(h => {alert(h.hashHex)})
+.catch(e => console.error('Login did not work'))
+
+sk.send(buff,{binary:true});
+}
+
+
+
+      function changePassword(contact, oldPassword, newPassword, oldSeed, newSeed)
+      {
+        // same as login
+        let buff = new ArrayBuffer([1 + contact.length + 64 + 64]);
+
+/*
+        argon2.hash({
+          pass: password, salt: oldSeed,
+          time: 32, mem: 32768, hashLen: 64,
+          type: argon2.ArgonType.Argon2d
+        })
+        .then(h => {buff[1:65] = h.hashHex})
+        .catch(e => console.error('Signup did not work'))
+
+        argon2.hash({
+          pass: password, salt: newSeed,
+          time: 32, mem: 32768, hashLen: 64,
+          type: argon2.ArgonType.Argon2d
+        })
+        .then(h => {buff[1:65] = h.hashHex})
+        .catch(e => console.error('Signup did not work'))
+*/
+        sk.send(buff,{binary:true});
+      }
+
+
+      function submitTOTP(id, input)
+      {
+        // same as login
+        let buff = new ArrayBuffer([8 + 4]);
+        sk.send(buff,{binary:true});
+      }
+
+      function verifyUser(id)
+      {
+        let buff = new ArrayBuffer([8]);
+        for (var i = 0; i < 8; ++i)
+          buff[i] = (id >> 4) & 0xF
+        sk.send(id);
+      }
+
+      function initTOTP(id, input) {
+      }
+
+      function upl()
+      {
+        let photo = document.getElementById("image-file").files[0];  // file from input
+        let req = new XMLHttpRequest();
+        let formData = new FormData();
+
+        formData.append("photo", photo);                                
+        req.open("POST", '/upload/image');
+        req.send(formData);
+
+        data = req.recv()
+      }
+
+
+
+      imgInp.onchange = evt => {
+      console.log(evt)
+      console.log(imgInp.files)
+      const fi = imgInp.files
+      //upImg
+      for (let i = 0; i < fi.length; ++i) {
+        const div = document.createElement('div')
+        const span = document.createElement('span')
+        const img = document.createElement('img')
+        img.src=URL.createObjectURL(fi[i])
+        div.appendChild(img)
+        div.appendChild(span)
+        upImg.appendChild(div)
+        console.log(URL.createObjectURL(fi[i]))
+      }
+
+      taskpref.style.display="block"
+      imgInp.style.display="none"
+      uplList.style.display="none"
+
+      /*
+      const [file] = imgInp.files
+      console.log(file)
+      if (file) {
+        blah.src = URL.createObjectURL(file)
+      }*/
+    }
+
+/*
+    upImg.addEventListener('drop', handleDrop, false)
+
+    function handleDrop(e) {
+      let dt = e.dataTransfer
+      let files = dt.files
+      console.log(files)
+
+      //handleFiles(files)
+    }
+*/
+    window.onbeforeunload = function(){
+       // Do something
+    }
+
+    // OR
+    window.addEventListener("beforeunload", function(e){
+       // Do something
+    }, false);
+
+    document.addEventListener("keydown", (event) => {
+      console.log(event)
+      if (event.keyCode === 49)
+        e0.checked=true
+      if (event.keyCode === 50)
+        e1.checked=true
+      if (event.keyCode === 13) {
+        e0.checked=false
+        e1.checked=false
+      }
+    });
+
+    sq = []
+
+    function submit() {
+      if (sq.length<20){
+        sq.append([rating, e, comments])
+      }
+
+      alert('submitted')
+    }
+
+/*
+(function(fn = function() {
+  const page = useHash ?
+    window.location.hash.split('#').pop() :
+    window.location.href.split('/').pop();
+  get(routes.indexOf(page) >= 0 ? page : routes[0]);
+}) {
+  if (document.readyState != 'loading'){
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+})();*/
